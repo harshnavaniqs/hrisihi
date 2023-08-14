8cf9c2841453e03995d0d37a65b3ac0126463785
Hrishikesh-Panigrahi <hrishikesh.panigrahi38@gmail.com>
2023-08-14 08:16:58 +0530

    completed circle

--- src/components/TicketCard/TicketCard.jsx
+++ src/components/TicketCard/TicketCard.jsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TicketCard.css";
 
-function TicketCard({ task }) {
+function TicketCard({ task, users }) {
+  const [isCompleted, setIsCompleted] = useState(task.completed || false);
+
+  const handleToggleComplete = () => {
+    setIsCompleted(!isCompleted);
+  };
+
   return (
-    <div className="ticket-card">
-      <div className="card-body">
-        {task.id}
-        <h3 className="title"> {task.title}</h3> {/* Display id as title */}
-        <p className="tag">
-          {" "}
-          <span className="tag-text">{task.tag}</span>
-        </p>
+    <div className={`ticket-card ${isCompleted ? "completed" : ""}`}>
+      <div className={`circle ${isCompleted ? 'completed' : ''}`} onClick={handleToggleComplete}>
+      {/* {isCompleted ? '✓' : ''} */}
+     
+        {isCompleted && <span className="tick">✔</span>}
+      </div>
+      <div className="ticket-details">
+        <div className="card-body">
+          {task.id}
+          <h3 className="title"> {task.title}</h3> {/* Display id as title */}
+          <p className="tag">
+            {" "}
+            <span className="tag-text">{task.tag}</span>
+          </p>
+        </div>
       </div>
     </div>
   );
